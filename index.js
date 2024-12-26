8a16b28de93f35047a76e37ee04f93ba2bd9e551
Salmanshaik-03 <salmanshaik032000@gmail.com>
2024-12-26 07:49:42 +0530

    1st

---
+++ index.js
@@ -0,0 +1,20 @@
+let num=prompt("Enter a number");
+if(num%2==0){
+    alert(`Even`);
+}
+else{
+    alert(`Odd`);
+}
+
+
+let num1=prompt("Enter the first number");
+let num2=prompt("Enter the second number");
+if(num1>num2){
+    alert(`Greater`);
+}
+else if(num1==num2){
+    alert(`Equal`)
+}
+else if(num1<num2){
+    alert(`Smaller`)
+}
